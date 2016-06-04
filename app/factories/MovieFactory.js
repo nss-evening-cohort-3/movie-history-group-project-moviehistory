4a8c553b750e8efb01cd35753fb61ec826f0fec4
Odie Joseph <Odie Joseph>
2016-06-03 20:02:56 -0500

    Initial commit beginning of app

---
+++ app/factories/MovieFactory.js
@@ -0,0 +1,26 @@
+						// *****MOVIE HISTORY INSTRUCTIONS*****
+
+// Must have the ability to register a user in Firebase
+// Must have the ability to log in
+	// Create a function (that gets called on click) that can be called from this factory that is going to get the users email and password they use once they hit the "register" button on the Login Screen and stores that information in Firebase then logges them in on the same click.
+
+
+// You must use Firebase to store movies
+	// Create a function that stores movies into Firebase. Find out where the movies are coming from.
+
+
+// You must be able to add movies
+	// This function will most likely be called on click when the user adds the movie to their MovieList or gives it a rating. The function would get the ID of the movie that is chosen then add it to the movie list.
+
+// You must be able to remove movies
+	// This function will be called on click of the delete button that is on the movielist. It should get the ID of the movie that is chosen and remove it.
+
+
+// Each movie must have the following properties:
+	// The following properties will be in an object associated with the movie and its title. This should be the same format as the JSON of the API where we are getting this information.
+	
+		// Movie name 
+		// Year released
+		// An array of major actors
+		// An integer rating of 1-5
+		// A boolean value that, if true, means that you have watched the movie
