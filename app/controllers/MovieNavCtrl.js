d5f14ab91e3dd19d9f013359a64dd5bb2428319b
Paul <wip321@yahoo.com>
2016-06-08 20:02:32 -0500

    loading from firebase and posting to firebase working.

--- app/controllers/MovieNavCtrl.js
+++ app/controllers/MovieNavCtrl.js
@@ -1,22 +1,24 @@
 app.controller("MovieNavCtrl", function($scope, $routeParams, MovieFactory){
 
-  $scope.listItems = {}
-  $scope.hideFire = true
-  $scope.hideFireMovies = true
-  MovieFactory.getFirebaseMovies().then(function(stuff) {
-    $scope.hidefire = false
-    console.log(stuff)
-  })
+  $scope.listItems = {};
+  $scope.hideFire = true;
+
   $scope.toWatch = function() {
-    $scope.hideFireMovies = true
+    MovieFactory.getFirebaseMovies().then(function(stuff) {
+    $scope.hideFire = false;
+    $scope.listItems = stuff;
+  })
   }
   $scope.watched = function() {
-    $scope.hideFireMovies = false
+    MovieFactory.getFirebaseMovies().then(function(stuff) {
+    $scope.hideFire = false;
+    $scope.listItems = stuff;
+  })
   }
 	// this fuction uses the factory to get the movies from the API url and then attaches them to a variable called "stuff" which holds all of the object data. You then attached "stuff" to the listItems empty object to make available to the partial html files in order to go through them and put on the DOM. 
 	$scope.search = function(title) {
     MovieFactory.getMovies(title).then(function(stuff) {
-      $scope.listItems = stuff;
+      $scope.listItems = stuff.Search;
       console.log("stuff", stuff);
     })
   }
