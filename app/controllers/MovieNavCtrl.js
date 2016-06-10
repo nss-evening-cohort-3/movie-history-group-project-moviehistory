3729788fc7d0ac0c29ee093138a33890141eb505
Paul <wip321@yahoo.com>
2016-06-10 12:58:24 -0500

    small edits

--- app/controllers/MovieNavCtrl.js
+++ app/controllers/MovieNavCtrl.js
@@ -5,12 +5,14 @@ app.controller("MovieNavCtrl", function($scope, $routeParams, MovieFactory){
   $scope.hideFirebase = true;
 
   $scope.toWatch = function() {
+    $scope.listItems = {};
     MovieFactory.getFirebaseMovies().then(function(stuff) {
     $scope.hideFire = false;
     $scope.listItems = stuff;
   })
   }
   $scope.watched = function() {
+    $scope.listItems = {};
     MovieFactory.getFirebaseMovies().then(function(stuff) {
     $scope.hideFire = false;
     $scope.listItems = stuff;
@@ -18,6 +20,8 @@ app.controller("MovieNavCtrl", function($scope, $routeParams, MovieFactory){
   }
 	// this fuction uses the factory to get the movies from the API url and then attaches them to a variable called "stuff" which holds all of the object data. You then attached "stuff" to the listItems empty object to make available to the partial html files in order to go through them and put on the DOM. 
 	$scope.search = function(title) {
+    $scope.listItems = {};
+    $scope.hideFire = true;
     MovieFactory.getMovies(title).then(function(stuff) {
       $scope.listItems = stuff.Search;
       console.log("stuff", stuff);
